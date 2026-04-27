b4caae279f2da1a7e40bcf2f9e5401f03f411bca
Shafi.inz <shafiineza25@gmail.com>
2026-04-27 17:34:13 +0200

    Add files via upload

--- ILLIGATION.simple25.ino
+++ ILLIGATION.simple25.ino
@@ -1,18 +1,32 @@
 
-const int soilSensor = A0;  
-const int pumpPin = 7;       
-int dryValue = 600;  
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+const int soilSensor = A0;
+const int pumpPin = 7;
+int dryValue = 600;
 void setup() {
   pinMode(pumpPin, OUTPUT);
   pinMode(soilSensor,INPUT);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Irrigation Sys");
+  delay(2000);
+  lcd.clear();
 }
 void loop() {
   int moisture = analogRead(soilSensor);
-  if (moisture > dryValue) {
+  lcd.setCursor(0, 0);
+  lcd.print("Moisture: ");
+  if (moisture > dryValue){
     digitalWrite(pumpPin, LOW);
-  } 
-  else {
-    digitalWrite(pumpPin, HIGH);
+    lcd.setCursor(0, 1);
+    lcd.print("Pump:OFF");
+  } else {
+    digitalWrite(pumpPin,HIGH);
+    lcd.setCursor(0, 1);
+    lcd.print("Pump:ON");
   }
   delay(1000);
 }
